f8c3c35f9be96eae72c37d74850db5d2c017c9f1
boran <borangoher@proton.me>
2024-11-11 15:09:30 +0300

    feat: Allow cors from any origin

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var cors = require("cors");
 var parametersRouter = require("./routes/parameters");
 
 // set up app
@@ -10,6 +11,11 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/parameters", parametersRouter);
